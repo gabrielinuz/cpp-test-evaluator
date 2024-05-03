6c3314c6411053675fc2798e64b02645a400ecca
gabrielinuz <gabrielinuz@gmail.com>
2024-05-03 18:42:40 -0300

    main modified

--- main.cpp
+++ main.cpp
@@ -10,13 +10,23 @@ using namespace std;
 
 int main() 
 {
+    /**
+     * @brief make_unique construye un objeto de tipo TestEvaluator
+     * lo envuelve en un std::unique_ptr.
+     * std::unique_ptr es un puntero inteligente
+     * que posee y administra otro objeto
+     * a través de un puntero y elimina ese objeto
+     * cuando unique_ptr sale del alcance.
+     */
     auto te = make_unique<TestEvaluator>();
 
     //Imprimir el título del Caso de Pruebas
     te->title("Math operation tests:");
 
     // Realizar pruebas unitarias
-    te->evaluate("2 + 2 == 4", 2 + 2 == 4);
+    te->evaluate("Evaluando que 2 + 2 es cuatro", 2 + 2 == 4);
+
+    // Realizar pruebas usando la función macro
     ASSERT(3 * 7 == 21);
     ASSERT(3 * 7 == 24);// Esta prueba fallará
     ASSERT(5 - 2 == 4); // Esta prueba fallará
